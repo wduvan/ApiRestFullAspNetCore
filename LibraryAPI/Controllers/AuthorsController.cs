fc3fe83c85bbe9b075a52b7081d0bfac8abb96db
Duvan <wilsonduvan@gmail.com>
2020-05-29 18:53:47 -0500

    Add first endpoint and migration

---
+++ LibraryAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace LibraryAPI.Controllers
+{
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet("api/authors")]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ApiRestFullAspNetCore
 This is my first API Rest Full API using ASP Net Core 3
+
+To run project please configure the next Envairoments Variables
+
+#### Installation Library Api
+
+To run the project in the first time is necessary set the _environment variables_
+
+```shell script
+API_DB={api_db};
+API_PASS_DB={api_pass_db};
+API_USER_DB={api_user_db};
+``` 
+
+This app is development by [wduvan](http://www.wduvan.com)
