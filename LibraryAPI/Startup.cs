671165414d67ba9a39625a84c91560811e7b5ca1
Duvan <wilsonduvan@gmail.com>
2020-06-03 18:55:57 -0500

    add support to xml and json, just add to headers acept parametrer

--- LibraryAPI/Startup.cs
+++ LibraryAPI/Startup.cs
@@ -23,7 +23,12 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
+                setupAction.OutputFormatters.Add(
+                    new Microsoft.AspNetCore.Mvc.Formatters.XmlDataContractSerializerOutputFormatter());
+            }).AddXmlDataContractSerializerFormatters();
 
             services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
 
